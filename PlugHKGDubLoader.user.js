bd691f68a07220f9acc2cba7dd48b333a22a001c
fukingbus <kennethwu35@gmail.com>
2015-10-26 02:13:38 +0800

    renaming2

--- PlugHKGDubLoader.user.js
+++ PlugHKGDubLoader.user.js
@@ -4,21 +4,16 @@
 // @description  PlugHKG iconpack loader for dubtrack
 // @author       Go2SchoolByBus
 // @match        https://www.dubtrack.fm/join/plug-hkg
-// @source https://raw.githubusercontent.com/fukingbus/plughkgdub/master/PlugHKGDubLoader.js
+// @source https://raw.githubusercontent.com/fukingbus/plughkgdub/master/PlugHKGDubLoader.user.js
 // ==/UserScript==
 
  function load(){
  	 var itv = window.setInterval(function(){
         if($('#main-loading').css('display') != 'none'){
             window.clearInterval(itv);
-            jQuery.getScript('https://bus.wtako.net/plugdj/Script/PlugHKGDub.js');
+            jQuery.getScript('https://bus.wtako.net/plugdj/Script/PlugHKGDub.user.js');
                /**
                 	 -------------------------------------------------------------------------------------
-                	                 用家你好，若果你能夠看到這段訊息，證明你的步驟沒有錯             
-                	                     **上面的錯誤提示只會出現於插件載入失敗時**                   
-                	                                 你下一步應該做的事 :                             
-                	   按下 Ctrl + A ，再按 Ctrl +C ，再依據影片所示貼上於 Tampermonkey 或 Greasemonkey  
-                	                                                                                  
                 	                       感謝您支持PlugHKG 高登音樂台，祝您使用愉快                        
                 							    	Ⓒ Go2SchoolByBus 2015
                 	                       Facebook Page:       http://fb.com/plughkg                 
--- README.md
+++ README.md
@@ -12,11 +12,19 @@ https://www.dubtrack.fm/join/plug-hkg
 1. 請先安裝插件管理擴充程式
    * Chrome 用戶應安裝 [Tempermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
    * FireFox 用戶應安裝 [Greasemonkey](https://addons.mozilla.org/en-us/firefox/addon/greasemonkey/)
-2. 安裝完成後，請按 [這裡](https://raw.githubusercontent.com/fukingbus/plughkgdub/master/PlugHKGDubLoader.js) 安裝插件
+2. 安裝完成後，請按 [這裡](https://raw.githubusercontent.com/fukingbus/plughkgdub/master/PlugHKGDubLoader.user.js) 安裝插件
+3. 插件管理擴充程式Tempermonkey或者Greasemonkey應該會提示閣下安裝，若沒有，請重新檢查安裝程序或依照以下步驟手動安裝
 
-
+# Chrome with Tempermonkey 手動安裝
+  [![CHROME w/ Tempermonkey](http://img.youtube.com/vi/qXcjUC71LbQ/0.jpg)](http://www.youtube.com/watch?v=qXcjUC71LbQ)
+# Firefox with Greasemonkey手動安裝
+  [![FIREFOX w/ Greasemonkey](http://img.youtube.com/vi/DFkg4FczSRs/0.jpg)](http://www.youtube.com/watch?v=DFkg4FczSRs)
 
 ## 更新歷程 Changelog 
+
+26 OCT 2015 / v0.2
++ One-Click install
++ readme update
 
 26 OCT 2015 / v0.1
 + HKG icon(s) implement
