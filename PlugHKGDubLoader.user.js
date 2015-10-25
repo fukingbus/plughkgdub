40e9588cc3f15f4105512f457349dc10ee26be1c
fukingbus <kennethwu35@gmail.com>
2015-10-26 02:39:31 +0800

    renaming

--- PlugHKGDubLoader.user.js
+++ PlugHKGDubLoader.user.js
@@ -11,7 +11,7 @@
  	 var itv = window.setInterval(function(){
         if($('#main-loading').css('display') != 'none'){
             window.clearInterval(itv);
-            jQuery.getScript('https://bus.wtako.net/plugdj/Script/PlugHKGDub.user.js');
+            jQuery.getScript('https://bus.wtako.net/plugdj/Script/PlugHKGDub.js');
                /**
                 	 -------------------------------------------------------------------------------------
                 	                       感謝您支持PlugHKG 高登音樂台，祝您使用愉快                        
